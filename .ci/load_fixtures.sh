ebc53babd05f3c4cf1ded8646db53b75a976c44e
Mike Baynton <mbaynton@umn.edu>
2017-05-25 09:40:27 -0500

    Debugging CI error; print netstat and tell ldapadd to give more debug output.

--- .ci/load_fixtures.sh
+++ .ci/load_fixtures.sh
@@ -4,8 +4,10 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 FIXTURES_DIR="$DIR/ldif"
 
 load_fixture () {
-  ldapadd -x -H ldap://127.0.0.1:3890/ -D "cn=Manager,dc=example,dc=com" -w insecure -f $1
+  ldapadd -d 12 -x -H ldap://127.0.0.1:3890/ -D "cn=Manager,dc=example,dc=com" -w insecure -f $1
 }
+
+netstat -lp
 
 for FIXTURE in `ls ${FIXTURES_DIR}`
 do
