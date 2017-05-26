798ed9c5d8f9665f12b497a8e7361bf4621bd1d8
Mike Baynton <mbaynton@umn.edu>
2017-05-26 15:58:34 -0500

    Print the supported SASL mechs for funsies

--- .ci/load_fixtures.sh
+++ .ci/load_fixtures.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 FIXTURES_DIR="$DIR/ldif"
 
 load_fixture () {
-  ldapadd -d 12 -x -H ldap://127.0.0.1:3890/ -D "cn=Manager,dc=example,dc=com" -w insecure -f $1
+  ldapadd -x -H ldap://127.0.0.1:3890/ -D "cn=Manager,dc=example,dc=com" -w insecure -f $1
 }
 
 for FIXTURE in `ls ${FIXTURES_DIR}`
@@ -17,4 +17,5 @@ export LDAPTLS_REQCERT=never
 export LDAPTLS_CERT=/tmp/client-cert.pem
 export LDAPTLS_KEY=/tmp/client-key.pem
 
+ldapsearch-x -H ldap://127.0.0.1:3890/ -D "cn=Manager,dc=example,dc=com" -w insecure -s base -LLL supportedSASLMechanisms
 ldapsearch -d "-1" -Y EXTERNAL -H ldaps://localhost:6360 -b dc=example,dc=com '(uid=user1)' dn
